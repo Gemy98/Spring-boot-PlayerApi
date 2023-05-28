672e9bb99dcbc42d3dbe62720666f5718e7a7621
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-28 07:34:26 +0300

    prevent auto API created by JPA

--- src/main/java/com/spring/player/controller/PlayerController.java
+++ src/main/java/com/spring/player/controller/PlayerController.java
@@ -3,18 +3,15 @@ package com.spring.player.controller;
 import com.spring.player.model.Player;
 import com.spring.player.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//localhost:8080/api/
+//localhost:8080/
 @RestController
-//localhost:8080/api/C
-@RequestMapping("/C")
+//localhost:8080/api/
+@RequestMapping("/myapi")
 public class PlayerController {
 
 
@@ -26,18 +23,92 @@ public class PlayerController {
     PlayerService playerService ;
 
 
-    //localhost:8080/C/getplayer
+    //localhost:8080/api/getplayer
     @GetMapping("/getplayer")
     private Player getPlayer(@RequestParam String name){
         return playerService.getPlayerByName(name);
     }
 
-    //localhost:8080/C/getallplayers
+    //localhost:8080/api/getallplayers
     @GetMapping("/getallplayers")
     private List<Player> getAllPlayers(@RequestParam String name){
         List<Player> players = new ArrayList<>();
         players = playerService.getAllPlayersByName(name);
         return players ;
     }
+
+
+    //localhost:8080/api/players
+    @GetMapping("/player")
+    public Player player(@RequestParam int id){
+        Player p = playerService.showPlayer(id);
+        if (p == null) {
+            throw new RuntimeException("player not found id :"+id);
+        }
+
+
+        return p;
+    }
+
+
+    //localhost:8080/api/myplayers
+    @GetMapping("/myplayers")
+    public List<Player> players(){
+
+        return playerService.allPlayers()	;
+    }
+
+    //localhost:8080/api/players
+    @PostMapping("/players")
+    public String savePlayer(@RequestBody Player player) {
+
+        playerService.savePlayer(player);
+        return"Successfully added";
+    }
+
+    //localhost:8080/api/players
+    @PutMapping("/players")
+    public Player updatePlayer(@RequestBody Player player) {
+        playerService.savePlayer(player);
+        return player ;
+    }
+
+
+	/*
+	//localhost:8080/api/players
+			@GetMapping("/deleteplayer")
+			public String deletePlayer(@RequestParam int id){
+				Player p = playerService.showPlayer(id);
+				if (p == null) {
+					throw new StudentException("player not found id :"+id);
+				}
+
+				playerService.deletePlayer(p);
+
+				return "Successfully Deleted id :"+ id ;
+
+			}
+		*/
+
+    //localhost:8080/api/players
+    @DeleteMapping("/player")
+    public String deletePlayer(@RequestParam int id){
+        Player p = playerService.showPlayer(id);
+        if (p == null) {
+            throw new RuntimeException("player not found id :"+id);
+        }
+
+        playerService.deletePlayer(p);
+
+        return "Successfully Deleted id :"+ id ;
+
+    }
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/spring/player/service/PlayerService.java
+++ src/main/java/com/spring/player/service/PlayerService.java
@@ -12,19 +12,44 @@ public class PlayerService {
 
     @Autowired
     public PlayerService(PlayerDAO playerDAO) {
-        this.playerDAO = playerDAO;
+        this.playerDao = playerDAO;
     }
 
-    private PlayerDAO playerDAO ;
+    private PlayerDAO playerDao ;
 
     public Player getPlayerByName(String name){
-    return playerDAO.findByName(name);
+    return playerDao.findByName(name);
     }
 
     public List<Player> getAllPlayersByName(String name){
 
-        List<Player> players = playerDAO.findAllByName(name);
+        List<Player> players = playerDao.findAllByName(name);
         return players ;
     }
+
+
+    public List<Player> allPlayers() {
+
+
+        return playerDao.findAll();
+    }
+
+
+    public void savePlayer(Player player) {
+        playerDao.save(player);
+    }
+
+    public Player showPlayer(int id) {
+
+        return 	playerDao.findById(id).get();
+    }
+
+    public void deletePlayer(Player player) {
+
+        int id = player.getId();
+        playerDao.deleteById(id);
+    }
+
+
 
 }
